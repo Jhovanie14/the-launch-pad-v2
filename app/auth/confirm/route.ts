a47a4692345ba4168f03419330fcbc215e7da3b8
Jhovanie14 <flores.jhovanie14@gmail.com>
2025-10-06 01:09:19 +0800

    auth/confirm

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -9,7 +9,6 @@ export async function GET(request: NextRequest) {
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = "/dashboard";
 
-  // Create redirect link without the secret token
   const redirectTo = request.nextUrl.clone();
   redirectTo.pathname = next;
   redirectTo.searchParams.delete("token_hash");
@@ -26,6 +25,7 @@ export async function GET(request: NextRequest) {
       redirectTo.searchParams.delete("next");
       return NextResponse.redirect(redirectTo);
     }
+
     if (error) {
       console.error("Email verification error:", error);
       redirectTo.pathname = "/error";
@@ -34,7 +34,6 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  // return the user to an error page with some instructions
   redirectTo.pathname = "/error";
   return NextResponse.redirect(redirectTo);
 }
