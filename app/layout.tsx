bfb92208e5b54ff5e2607d9574c4da05816bd4dd
Primary User <flores.jhovanie14@gmail.com>
2026-04-17 23:37:22 +0800

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,6 +95,27 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1525799285811252');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=1525799285811252&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-N38CCY2FTY"
           strategy="afterInteractive"
